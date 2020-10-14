cd37199803ae65cff5a4e6405d154973fb9b0604
saifaldin14 <saifaldindeveloper@gmail.com>
2020-10-14 13:52:23 -0400

    Update slider

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
 // var canvas = document.querySelector('canvas');
 // var ctx = canvas.getContext('2d');
 
-var weightSlider = document.getElementById("weight");
-var angleSlider = document.getElementById("angle");
-var frictionSlider = document.getElementById("friction");
-var sliders = [weightSlider, angleSlider, frictionSlider]
+const setSlider = () => {
+  var weightSlider = document.getElementById("weight");
+  var angleSlider = document.getElementById("angle");
+  var frictionSlider = document.getElementById("friction");
+  var sliders = [weightSlider, angleSlider, frictionSlider]
 
 
-var weightLabel = document.getElementById("weightLabel");
-var angleLabel = document.getElementById("angleLabel");
-var frictionLabel = document.getElementById("frictionLabel");
-var labels = [weightLabel, angleLabel, frictionLabel]
+  var weightLabel = document.getElementById("weightLabel");
+  var angleLabel = document.getElementById("angleLabel");
+  var frictionLabel = document.getElementById("frictionLabel");
+  var labels = [weightLabel, angleLabel, frictionLabel]
 
-for (var i = 0; i < sliders.length; i++) {
-  labels[i].innerHTML = sliders[i].value;
-}
+  for (var i = 0; i < sliders.length; i++) {
+    labels[i].innerHTML = sliders[i].value;
+  }
 
-sliders[0].oninput = function () {
-  labels[0].innerHTML = this.value;
+  sliders[0].oninput = function() {
+    labels[0].innerHTML = this.value;
+  }
+  sliders[1].oninput = function() {
+    labels[1].innerHTML = (this.value);
+  }
+  sliders[2].oninput = function() {
+    labels[2].innerHTML = (this.value);
+  }
 }
-sliders[1].oninput = function () {
-  labels[1].innerHTML = (this.value);
-}
-sliders[2].oninput = function () {
-  labels[2].innerHTML = (this.value);
-}
+setSlider();
 
 var canvas = document.querySelector('canvas');
 var c = canvas.getContext('2d');
