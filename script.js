49fabd2ff547f1c1e2564050d8196302997f668d
saifaldin14 <saifaldindeveloper@gmail.com>
2020-10-14 17:08:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -51,59 +51,31 @@ c.drawImage(SBImg, 50, 300);
 // canvas.width = window.innerWidth;
 // canvas.height = window.innerHeight;
 
-// const setSlider = () => {
-//   var weightSlider = document.getElementById("weight");
-//   var angleSlider = document.getElementById("angle");
-//   var frictionSlider = document.getElementById("friction");
-//   var sliders = [weightSlider, angleSlider, frictionSlider]
+//---------------------------
+// Functions to draw shapes
+// Useful to animate later
+// Plan: draw a rectangle to represent
+// the ramp and fill it with the image of wood
+//---------------------------
+const drawRect = (x, y, width, height) => {
+  ctx.fillStyle = 'rgba(255, 0, 0, 0.1)'
+  ctx.fillRect(x, y, width, height);
+}
 
+const drawLine = (originX, originY, destX, destY) => {
+  ctx.beginPath();
+  ctx.moveTo(originX, originY);
+  ctx.lineTo(destX, destY);
+  ctx.strokeStyle = "#fa34a3"
+  ctx.stroke();
+}
 
-//   var weightLabel = document.getElementById("weightLabel");
-//   var angleLabel = document.getElementById("angleLabel");
-//   var frictionLabel = document.getElementById("frictionLabel");
-//   var labels = [weightLabel, angleLabel, frictionLabel]
-
-//   for (var i = 0; i < sliders.length; i++) {
-//     labels[i].innerHTML = sliders[i].value;
-//   }
-
-//   sliders[0].oninput = function() {
-//     labels[0].innerHTML = this.value;
-//   }
-//   sliders[1].oninput = function() {
-//     labels[1].innerHTML = (this.value);
-//   }
-//   sliders[2].oninput = function() {
-//     labels[2].innerHTML = (this.value);
-//   }
-// }
-// setSlider();
-
-// //---------------------------
-// // Functions to draw shapes
-// // Useful to animate later
-// // Plan: draw a rectangle to represent
-// // the ramp and fill it with the image of wood
-// //---------------------------
-// const drawRect = (x, y, width, height) => {
-//   ctx.fillStyle = 'rgba(255, 0, 0, 0.1)'
-//   ctx.fillRect(x, y, width, height);
-// }
-
-// const drawLine = (originX, originY, destX, destY) => {
-//   ctx.beginPath();
-//   ctx.moveTo(originX, originY);
-//   ctx.lineTo(destX, destY);
-//   ctx.strokeStyle = "#fa34a3"
-//   ctx.stroke();
-// }
-
-// const drawCircle = (x, y, r) => {
-//   ctx.beginPath();
-//   ctx.arc(x, y, r, 0, Math.PI * 2, false);
-//   ctx.strokeStyle = 'blue';
-//   ctx.stroke();
-// }
+const drawCircle = (x, y, r) => {
+  ctx.beginPath();
+  ctx.arc(x, y, r, 0, Math.PI * 2, false);
+  ctx.strokeStyle = 'blue';
+  ctx.stroke();
+}
 
 // //---------------------------
 // // Functions to animate shapes
